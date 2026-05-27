fd9bfe00bc2e21c49e84ec83866c1dfe09629b61
umarmalikhj <umarmalik.hj@gmail.com>
2026-05-27 08:10:39 +0530

    Added Code

--- script.js
+++ script.js
@@ -8,7 +8,12 @@ function myFunction() {
 }
 
 const findExcuse = document.getElementById("excuse");
+const jointheGrind = document.getElementById("join");
 
 findExcuse.addEventListener("click", () => {
-    window.location.href = ("findexcuses.html");
+    window.location.href = "findexcuses.html";
+})
+
+jointheGrind.addEventListener("click", () => {
+    window.location.href = "jointhegrind.html";
 })
